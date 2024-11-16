55c23386cc7b8bafd1048ccdb5efa600b59e92de
vyacheslavbotoev <botoevslava27032003@mail.ru>
2024-11-16 19:18:00 +0300

    useAction abstraction logic

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,18 +1,30 @@
 "use client"
 
-import {create} from "@/actions/create-board";
-import {useFormState} from "react-dom"
+import {CreateBoard} from "@/actions/create-board/schema";
+
 import {FormInput} from "@/app/(platform)/(dashboard)/organization/[organizationId]/form-input";
 import {FormButton} from "@/app/(platform)/(dashboard)/organization/[organizationId]/form-button";
+import {createBoard} from "@/actions/create-board";
+import {useAction} from "@/hooks/useAction";
 
 export const Form = () => {
-    const initialState = { message: null, errors: {} }
-    const [state, dispatch] = useFormState(create, initialState);
+    const {execute, fieldErrors} = useAction(createBoard, {
+        onSuccess: (data) => {
+            console.log(data, "Success");
+        },
+        onError: (error) => {
+            console.error(error, "Error");
+        }
+    });
+    const onSubmit = (formData: FormData) => {
+        const title = formData.get("title") as string;
+        execute({title})
+    }
 
     return (
-        <form action={dispatch}>
+        <form action={onSubmit}>
             <div className="flex flex-col space-y-2">
-                <FormInput errors={state?.errors}/>
+                <FormInput errors={fieldErrors}/>
             </div>
             <FormButton/>
         </form>
