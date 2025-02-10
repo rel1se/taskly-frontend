40790df86c8389fdcac428cf9452f5b82ac72b3d
vyacheslavbotoev <botoevslava27032003@mail.ru>
2025-02-11 00:38:28 +0800

    added card copy and delete actions, card description

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -6,6 +6,8 @@ import {useQuery} from "@tanstack/react-query";
 import {CardWithList} from "@/types";
 import {fetcher} from "@/lib/fetcher";
 import {Header} from "@/components/modals/card-modal/header";
+import {Description} from "@/components/modals/card-modal/description";
+import {Actions} from "@/components/modals/card-modal/actions";
 
 export const CardModal = () => {
     const {id, isOpen, onClose} = useCardModal(state => state)
@@ -25,6 +27,22 @@ export const CardModal = () => {
                     ? <Header.Skeleton/>
                     : <Header data={cardData}/>
                 }
+                <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+                    <div className="col-span-3">
+                        <div className="w-full space-y-6">
+                            {!cardData ?
+                                <Description.Skeleton/>
+                                :
+                                <Description data={cardData}/>
+                            }
+                        </div>
+                    </div>
+                    {!cardData ?
+                        <Actions.Skeleton/>
+                        :
+                        <Actions data={cardData}/>
+                    }
+                </div>
             </DialogContent>
         </Dialog>
     )
