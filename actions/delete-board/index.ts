14ef45ec72bc8bd7b7f19081140e1365d2d0f9aa
vyacheslavbotoev <botoevslava27032003@mail.ru>
2025-02-19 14:28:36 +0300

    bug fix

--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -9,9 +9,11 @@ import {redirect} from "next/navigation";
 import {createAuditLog} from "@/lib/create-audit-log";
 import {ACTION, ENTITY_TYPE} from "@prisma/client";
 import {decreaseAvailableCount} from "@/lib/org-limit";
+import {checkSubscription} from "@/lib/subscription";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
     const {userId, orgId} = auth()
+    const isPro = await checkSubscription()
 
     if (!userId || !orgId) {
         return {
@@ -28,7 +30,10 @@ const handler = async (data: InputType): Promise<ReturnType> => {
                 orgId
             },
         })
-        await decreaseAvailableCount()
+
+        if (!isPro) {
+            await decreaseAvailableCount()
+        }
         await createAuditLog({
             entityId: board.id,
             entityTitle: board.title,
